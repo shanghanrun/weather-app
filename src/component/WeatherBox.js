95fb992883a4f55eb71f24d3d48557cc7d63624e
shanghanrun <idim7@naver.com>
2024-03-08 12:45:12 +0900

    .

--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const WeatherBox = ({weather}) => {
-  const f = weather? (weather.main.temp *1.8 +32).toFixed(2) : null;
+  const f = weather && weather.main? (weather.main.temp *1.8 +32).toFixed(2) : null;
   return (
     <div className='weather-box'>
         <div>{weather?.name}</div>
-        <h2>{weather?.main.temp}C / {f}F</h2>
-        <h3>{weather?.weather[0].description}</h3>
+        <h2>{weather && weather.main?.temp}C / {f}F</h2>
+        <h3>{weather && weather[0]?.description}</h3>
     </div>
   )
 }
